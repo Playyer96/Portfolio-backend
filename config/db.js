5fa33e7e2dbf72059c296682b2d6c302027b11ac
Dani <dagugo.96@gmail.com>
2025-01-17 02:08:30 -0500

    [Update]: Envrionment fix

--- config/db.js
+++ config/db.js
@@ -6,23 +6,25 @@ const dbName = process.env.DB_NAME;
 let client;
 
 export async function connectToDb() {
-    if (client) return client.db(dbName); // Return the cached client instance
+    if (client) return client.db(dbName);
 
     if (!uri) {
         throw new Error('MongoDB URI is not set');
     }
 
     try {
+        console.log('Attempting to connect to MongoDB...');
         client = new MongoClient(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000,  // Timeout after 30 seconds
         });
 
         await client.connect();
         console.log('MongoDB connected');
         return client.db(dbName);
     } catch (error) {
-        console.error('Failed to connect to MongoDB:', error);
+        console.error('MongoDB connection error:', error.message);
         throw new Error('MongoDB connection failed');
     }
 }
